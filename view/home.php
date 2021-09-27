b78011e26df3c4cb079b0c0f2583dcd2228b90d1
Fernando Pinage <luizfernandoluck@hotmail.com>
2021-09-27 18:47:53 -0300

    atualizando eurecatech

---
+++ view/home.php
@@ -0,0 +1,74 @@
+<?php
+require_once "../layout/head.html";
+?>
+
+<div class="container">
+    <p class=" text-center fw-bold mt-2" style="font-size: 12px;">EMISSÃO DE NOTA FISCAL DE SERVIÇO ELETRÔNICA – NFS-E</p>
+    <hr>
+
+    <fieldset class="border">
+        <legend class="fw-bold" style="color: #2196F5; font-size: 13px;">PRESTADOR DE SERVIÇO</legend>
+
+        <p><label class="px-2" style="font-size: 13px;">CPF/CNPJ:</label> <label style="font-size: 12px;" class="fw-bold">37.785.861/0001-32</label></p>
+        <p><label class="px-2" style="font-size: 13px;">Inscrição:</label> <label style="font-size: 12px;" class="fw-bold"></label></p>
+        <p><label class="px-2" style="font-size: 13px;">Municipal:</label> <label style="font-size: 12px;" class="fw-bold">47132601</label></p>
+        <p><label class="px-2" style="font-size: 13px;">Contribuinte:</label> <label style="font-size: 12px;" class="fw-bold">LUIZ FERNANDO PINAGE COUTINHO 00496334220</label></p>
+
+    </fieldset>
+
+    <fieldset class="border" style="margin-top: 20px;">
+        <legend class="fw-bold" style="color: #2196F5; font-size: 13px;">TOMADOR DE SERVIÇOS</legend>
+
+        <p><label class="px-2" style="font-size: 13px;">CPF/CNPJ:</label> <label style="font-size: 12px;" class="fw-bold">35.576.769/0001-37</label><label class="px-2" style="font-size: 13px;">Razão Social:</label> <label style="font-size: 12px;" class="fw-bold">PROGRIDE DIGITAL LTDA</label></p>
+        <p><label class="px-2" style="font-size: 13px;">Inscrição:</label> <label style="font-size: 12px;" class="fw-bold"></label></p>
+        <p><label class="px-2" style="font-size: 13px;">Municipal:</label> <label style="font-size: 12px;" class="fw-bold">45059501</label><label class="px-2" style="font-size: 13px;">Municipal:</label><label style="font-size: 12px;" class="fw-bold">PROGRIDE DIGITAL LTDA</label></p>
+        <p><label class="px-2" style="font-size: 13px;">Logradouro:</label> <label style="font-size: 12px;" class="fw-bold">DJALMA BATISTA</label></p>
+        <p><label class="px-2" style="font-size: 13px;">Número:</label> <label style="font-size: 12px;" class="fw-bold">3000</label><label class="px-2" style="font-size: 13px;">Número:</label><label class="px-2" style="font-size: 13px;">Complemento:</label><label style="font-size: 12px;" class="fw-bold">LJ F CD AMAZONAS FLAT</label></p>
+        <p><label class="px-2" style="font-size: 13px;">CEP:</label> <label style="font-size: 12px;" class="fw-bold">69055-038</label><label class="px-2" style="font-size: 13px;">Bairro:</label><label style="font-size: 12px;" class="fw-bold">PARQUE 10 DE NOVEMBRO</label></p>
+        <p>
+        <div class="row mb-1">
+            <label for="inputEmail3" class="col-sm-1 " style="font-size: 13px; margin-left:10px;">Municipio:</label>
+            <div class="col-md-1">
+                <input type="email" class="form-control form-control-sm" id="inputEmail3">
+            </div>
+            </p>
+            <p><label class="px-2" style="font-size: 13px;">Estado:</label> <label style="font-size: 12px;" class="fw-bold">AM</label><label class="px-2" style="font-size: 13px;">Pais:</label><label style="font-size: 12px;" class="fw-bold">BRASIL</label></p>
+            <p><label class="px-2" style="font-size: 13px;">Telefone:</label> <label style="font-size: 12px;" class="fw-bold">41412140</label></p>
+            <p>
+            <div class="row mb-1">
+                <label for="inputEmail3" class="col-sm-1 " style="font-size: 13px; margin-left:10px;">E-Mail:</label>
+                <div class="col-md-3">
+                    <input type="email" class="form-control form-control-sm" value="SURPORTE@EMAIL.COM">
+                </div>
+            </div>
+            </p>
+            <p>
+            <div class="row mb-1">
+                <label for="inputEmail3" class="col-sm-1 " style="font-size: 11px; margin-left:10px; color:#2196F5;">Apelido:</label>
+                <div class="col-md-3">
+                    <input type="email" class="form-control form-control-sm">
+                </div>
+            </div>
+            </p>
+
+    </fieldset>
+    <fieldset class="border" style="margin-top: 20px;">
+        <legend class="fw-bold" style="color: #2196F5; font-size: 13px;">TRIBUTAÇÃO</legend>
+
+        <p><label class="px-2" style="font-size: 13px;">Serviço Prestado:</label> <label style="font-size: 12px;" class="fw-bold">No Município</label></p>
+
+
+    </fieldset>
+    <fieldset class="border" style="margin-top: 20px;">
+        <legend class="fw-bold" style="color: #2196F5; font-size: 13px;">SERVIÇO</legend>
+
+        <p><label class="px-2" style="font-size: 13px;">Serviço Prestado:</label> <label style="font-size: 12px;" class="fw-bold">No Município</label></p>
+
+
+    </fieldset>
+</div>
+
+<?php
+require_once "../layout/footer.html";
+
+?>
