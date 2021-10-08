bf1500f0c8a6ab9d501f94e48f5d561eb62151d8
FernandoPinage <luizfernandoluck@hotmail.com>
2021-10-07 20:10:49 -0400

    ok

--- view/home.php
+++ view/home.php
@@ -77,9 +77,10 @@ require_once "../layout/head.html";
                 <tr>
                     <td scope="col">
 
-                        <input type="number" class="form-control">
+                        <input type="text" class="form-control"><input class="btn btn-outline-secondary" type="submit" name="*" data-bs-toggle="modal" data-bs-target="#exampleModal">
 
                     </td>
+
                     <td scope="col">
 
                         <input type="number" class="form-control">
@@ -102,7 +103,29 @@ require_once "../layout/head.html";
 
             </tbody>
         </table>
-
+        <!-- Modal -->
+        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-lg">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                    <?php
+                        $hg = file_get_contents("https://servicodados.ibge.gov.br/api/v2/cnae/classes");
+                        echo "<pre>";
+                        var_dump(json_decode($hg));
+                        echo "</pre>";
+                    ?>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-primary">Save changes</button>
+                    </div>
+                </div>
+            </div>
+        </div>
 
 
     </fieldset>
